29e5a5b12874ca07436bd8def17bd7542ebd45d9
amandaegliite <amandaeglite2008@gmail.com>
2025-01-24 07:41:25 +0000

    v03

---
+++ geometrija1.py
@@ -0,0 +1,30 @@
+from math import sqrt
+print("Ievadiet 4 skaitļus, lai noskaidrotu nogriežņa garumu!")
+Xa = int(input())
+Ya = int(input())
+Xb = int(input())
+Yb = int(input())
+Xc = int(input())
+Yc = int(input())
+garums=((Xa-Xb)+(Ya-Yb)**2)
+print(garumsAB)
+garums=((Xa-Xc)+(Ya-Yc)**2)
+garums=((Xb-Xc)+(Yb-Yc)**2)
+if garumsAB>garumsAC and garumsAB>garumsBC
+if (garumsAB)=(garumsAC)+(garumsBC) and garumsAC=(garumsAB)+(garumsBC) and garumsBC=(garumsAB)+(garumsAC)
+print("Taisnleņķa trijstūris")
+
+elif garumsAC>garumsAB and garumsAC>garumsBC
+if (garumsAC)=(garumsAB)+(garumsBC) and garumsAB=(garumsAC)+(garumsBC) and garumsBC=(garumsAC)+(garumsAB)
+print("Taisnleņķa trijstūris")
+
+elif garumsBC>garumsAB and garumsBC>garumsAC
+if (garumsBC)=(garumsAB)+(garumsAC) and garumsAB=(garumsBC)+(garumsAC) and garumsAC=(garumsBC)+(garumsAB)
+print("Taisnleņķa trijstūris")
+
+else :
+print("Nav taisnleņķa trijstūris")
+
+
+
+
