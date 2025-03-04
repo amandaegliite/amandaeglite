044f9cf2f7c5a5170e731ef4544fdd1ca6934dbb
amandaegliite <amandaeglite2008@gmail.com>
2025-03-04 13:18:48 +0000

    v04

---
+++ README.md/svgatavosanas.py
@@ -0,0 +1,70 @@
+saraksts = [1, 2, 3, 4, 5]
+
+
+print (saraksts[0])
+print(saraksts[-1])
+
+for i in range (len(saraksts)):
+  (len(saraksts//2))
+ 
+print(saraksts[3])
+
+if saraksts == 13:
+ print("IR")
+else:
+  print("NAV")
+
+ 
+for i in range (len(saraksts)):
+  if i%2 != 0:
+    print(saraksts)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
